ffa59088cb904db8942fe1116d36dba84a4d7a95
Jim Marshall <46267679+JimMarshall35@users.noreply.github.com>
2022-11-02 17:39:58 +0000

    some exploratory code changes

--- sources/engine/Stride.Assets.Models/bf2Importer/BF2Importer.cs
+++ sources/engine/Stride.Assets.Models/bf2Importer/BF2Importer.cs
@@ -5,23 +5,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Stride.Importer.Common;
+using Stride.Assets.Models.bf2Importer.BFP4FExplorerWV;
 
 namespace Stride.Assets.Models.bf2Importer
 {
-    // TODO: MOVE bf2Importer namespace into a new project
-    // was having trouble sojust made a new sub folder / namespace in
+    // TODO: MOVE bf2Importer namespace into a new project under "90-Tools/3D-Importers"
+    // was having trouble doing so, so just made a new sub folder / namespace in
     // Stride.Assets.Models for now
     public static class BF2Importer
     {
+        /// <summary>
+        /// set an EntityInfo object's TextureDependencies
+        /// property based on a List of Helper.BF2MeshSTMGeometryMaterial
+        /// from a parsed BF2 staticmesh file
+        /// </summary>
+        /// <param name="entityInfo"></param>
+        /// <param name="materials"></param>
+        /// <returns></returns>
+        private static EntityInfo WithTextureDependencies(this EntityInfo entityInfo, BF2StaticMesh mesh, string modelPath)
+        {
+            // extract unique texture paths
+            var accumulatedTexturesList = new List<string>();
+            foreach (var mat in mesh.geomat)
+            {
+                foreach (var mat2 in mat.materials)
+                {
+                    accumulatedTexturesList = mat2.textureMapFiles.Aggregate(accumulatedTexturesList, (acc, next) =>
+                    {
+                        if (!acc.Contains(next))
+                        {
+                            acc.Add(next);
+                        }
+                        return acc;
+                    });
+                }
+            }
+            entityInfo.TextureDependencies = accumulatedTexturesList;
+            return entityInfo;
+        }
+
+        private static EntityInfo WithMaterials(this EntityInfo entityInfo, BF2StaticMesh mesh)
+        {
+            var numberOfEachKey = new Dictionary<string, int>();
+            entityInfo.Materials = new Dictionary<string, Materials.MaterialAsset>();
+            foreach (var mat in mesh.geomat)
+            {
+                foreach (var mat2 in mat.materials)
+                {
+                    string materialName = $"{mat2.technique}{mat2.shaderFile}";
+                    if (numberOfEachKey.ContainsKey(materialName))
+                    {
+                        numberOfEachKey[materialName]++;
+                    }
+                    else
+                    {
+                        numberOfEachKey[materialName] = 0;
+                    }
+                    var materialAsset = new Materials.MaterialAsset();
+                    // do stuff here to set MaterialAsset
+                    //materialAsset.Attributes.
+                    var name = $"{materialName}{mat2.technique}{numberOfEachKey[materialName]}";
+                    entityInfo.Materials[name] = materialAsset;
+                }
+            }
+            return entityInfo;
+        }
+
+        private static EntityInfo WithModels(this EntityInfo info, BF2StaticMesh mesh)
+        {
+            info.Models = new List<MeshParameters>();
+            for (int i=0; i < mesh.geometry.numGeom; i++)
+            {
+                var meshParams = new MeshParameters();
+                info.Models.Add(meshParams);
+            }
+            return info;
+        }
+
+
         private static EntityInfo ExtractStaticMeshEntityInfo(string filePath, bool extractTextureDependencies)
         {
-            var parsedMesh = new BFP4FExplorerWV.BF2StaticMesh(File.ReadAllBytes(filePath));
-            EntityInfo entityInfo = new EntityInfo();
-            return entityInfo;
+            var parsedMesh = new BF2StaticMesh(File.ReadAllBytes(filePath));
+            var entityInfo = new EntityInfo();
+
+            return new EntityInfo()
+                .WithTextureDependencies(parsedMesh, Path.GetDirectoryName(filePath))
+                .WithMaterials(parsedMesh)
+                .WithModels(parsedMesh);
 
         }
         private static EntityInfo ExtractBundledMeshEntityInfo(string filePath, bool extractTextureDependencies)
         {
+            var parsedMesh = new BF2BundledMesh(File.ReadAllBytes(filePath));
             EntityInfo entityInfo = new EntityInfo();
             return entityInfo;
 
